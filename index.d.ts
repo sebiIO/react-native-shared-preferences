5f0fcaf4fb6896f5e00d422bdfb190c9ff0826ed
Sebi <sebim254@gmail.com>
2022-01-29 01:39:43 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ declare module "react-native-shared-preferences" {
     const setName: (name: string) => void;
     const setItem: (key: string , value: string) => void;
     const getItem: (key: string, callback: GetItemCallback) => void;
-    const getMultipleItems: (keys: string[] , calback: GetMultipleItemsCallback) => void;
+    const getItems: (keys: string[] , calback: GetMultipleItemsCallback) => void;
     const getAll: (calback: GetMultipleItemsCallback) => void;
     const clear: () => void;
     const getAllKeys: (callback: GetMultipleItemsCallback) => void;
